a789c39d9e48d2ed789130d0847f17fb8d18a415
Tijani Moro <tjhackx111@gmail.com>
2023-10-10 01:34:30 +0000

    Add examples, shortcut method for Todos resource

---
+++ examples/basic-client.php
@@ -0,0 +1,15 @@
+<?php
+
+use Api\Client\ClientBuilder;
+use Api\Client\Sdk;
+use Http\Client\Common\Plugin\HeaderDefaultsPlugin;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$clientBuilder = new ClientBuilder();
+$clientBuilder->addPlugin(new HeaderDefaultsPlugin([
+    'Accept' => 'application/json',
+]));
+
+$sdk = new Sdk($clientBuilder);
+$response = $sdk->todos()->all();
